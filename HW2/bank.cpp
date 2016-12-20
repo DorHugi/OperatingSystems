17ef8013938aeb81bd2032db86c6487f16eeec7b
DorHugi <dorhugi@gmail.com>
2016-12-20 22:17:31 +0200

    need to commit before moving to ssh. nothing special

--- HW2/bank.cpp
+++ HW2/bank.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
-
-
+#include "atm.h"
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout << "argc is: " << argc << endl;
+
+
+    //Program arguements: bank <number of ATMs> <ATM1 input file> <ATM 2 > ..
+
+    
+    for (int i = 0; i < argc ; i++)
+        printf("The value of arg %d is %s \n",i,argv[i]);
+
+    // get the number of atms:
+    int atmNum = atoi(argv[1]);
+
+    //create a thread for each ATM.
+    pthread_t atmThreads[atmNum];
+    
+    char* x = "hello"; 
+    void* vx = (void*)(x);
+    createAtm(vx);
 
 return 0;
 }
