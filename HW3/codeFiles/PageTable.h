5a7ebaa95bc55cf5f3af0f435e2a8bb95ebad6eb
dorhugi@gmail.com <dorhugi@gmail.com>
2017-01-16 20:21:30 +0200

    Created the basic required files for HW3

---
+++ HW3/codeFiles/PageTable.h
@@ -0,0 +1,17 @@
+#include “PageDirectoryEntry.h”
+
+class PageTable
+
+{
+
+    public:
+
+    //Your Constructor (and Destructor if you need one) should go here
+
+    int* GetPage (unsigned int adr);
+
+    private:
+
+    //Fill the class with the necessary member variables
+
+};
