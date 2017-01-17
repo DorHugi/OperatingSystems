fc7511b67f276a19963cacc8fa792479cb7f1f1c
dorhugi@gmail.com <dorhugi@gmail.com>
2017-01-17 18:54:54 +0200

    Fixed test file

--- HW3/codeFiles/Test.cpp
+++ HW3/codeFiles/Test.cpp
@@ -1,34 +1,42 @@
-
 #include<fstream>
 #define VECSIZE 50
+
 using namespace std;
+
 int main(){
     VirtualMemory vrtlMem;
+
     OurPointer matBase = vrtlMem.OurMalloc(VECSIZE*VECSIZE);
     OurPointer vecBase = vrtlMem.OurMalloc(VECSIZE);
     OurPointer resBase = vrtlMem.OurMalloc(VECSIZE);
+
     OurPointer mat = matBase;
     OurPointer vec = vecBase;
     OurPointer res = resBase;
+
     ofstream matricesFile;
     matricesFile.open("matrices.txt");
     srand(1);
+
     for (int i = 0; i < VECSIZE * VECSIZE; ++i) {
         *(mat++) = rand() % 100;
     }
+
     mat = matBase; for (int i = 0; i < VECSIZE; ++i) {
         *(vec++) = rand() % 20000;
         *(res++) = 0;
     }
+
     vec = vecBase;
     res = resBase;
+
     for (int row = 0; row < VECSIZE; ++row) {
         for (int col = 0; col < VECSIZE; ++col) {
-            //*res += (*(mat++)) * (*(vec++)); //it works, but accesses to operator*
-            might happen in an unspecified order
+            //*res += (*(mat++)) * (*(vec++)); //it works, but accesses to operator* might happen in an unspecified order
             int mat_value = *(mat++); //our fix for log
             *res += (mat_value) * (*(vec++)); //log file consistency
         }
+
         vec = vecBase;
         ++res;
     }
@@ -36,17 +44,22 @@ int main(){
     vec = vecBase;
     res = resBase;
     matricesFile << "Matrix:" << endl;
+
     for (int row = 0; row < VECSIZE; ++row) {
         for (int col = 0; col < VECSIZE; ++col)
             matricesFile << *(mat++) << " ";
             matricesFile << endl;
     }
+
     matricesFile << "Vector:" << endl;
+
     for (int col = 0; col < VECSIZE; ++col)
         matricesFile << *(vec++) << " ";
-        matricesFile << endl;
-        matricesFile << "Result:" << endl;
-        for (int col = 0; col < VECSIZE; ++col)
-            matricesFile << *(res++) << " ";
-            matricesFile << endl;
+
+    matricesFile << endl;
+    matricesFile << "Result:" << endl;
+
+    for (int col = 0; col < VECSIZE; ++col)
+        matricesFile << *(res++) << " ";
+    matricesFile << endl;
 }
