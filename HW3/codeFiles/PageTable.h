ea50a1466e63635d0fd65dce6df336107bc3a29b
dorhugi@gmail.com <dorhugi@gmail.com>
2017-01-17 21:53:14 +0200

    Parsed the address

--- HW3/codeFiles/PageTable.h
+++ HW3/codeFiles/PageTable.h
@@ -1,13 +1,21 @@
-//#include <PageDirectoryEntry.h>
+#include <PageDirectoryEntry.h>
+
+
+
+//CONSTS:
+#define PAGE_DIRECTORY_ENTRIES 1024
+
 
 class PageTable
 {
     public:
 
     //Your Constructor (and Destructor if you need one) should go here
+	PageTable();
     int* GetPage (unsigned int adr); 
+
 
     private:
     //Fill the class with the necessary member variables
-
+    PageDirecotryEntry* directoryEntries[PAGE_DIRECTORY_ENTRIES];
 };
