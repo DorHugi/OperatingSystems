2deef42568863b8065f2201e3ad42142cef90aca
Inbal Orbach <orbach9@t2.technion.ac.il>
2017-01-17 23:11:37 +0200

    Compilation works

--- HW3/codeFiles/PageTable.h
+++ HW3/codeFiles/PageTable.h
@@ -5,10 +5,14 @@
 
 class PageTable
 {
+	friend class VirtualMemory;
+	
     public:
 
     //Your Constructor (and Destructor if you need one) should go here
-    int* GetPage (unsigned int adr); 
+    int* GetPage (unsigned int adr){};
+		PageTable(){};
+		~PageTable(){};
 
     private:
     //Fill the class with the necessary member variables
